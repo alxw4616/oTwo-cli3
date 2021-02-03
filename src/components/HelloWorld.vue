4e7bd6bb9f954ae7be9883d68cf246bff91662bf
alxw4616 <alxw4616@msn.com>
2021-02-03 08:47:07 +0800

    // 字体图标 http://www.fontawesome.com.cn/ // <i class="fa fa-address-book-o" aria-hidden="true"></i> import 'font-awesome/css/font-awesome.min.css'

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="hello">
+    <div>123123</div>
+    <div><i class="fa fa-address-book-o" aria-hidden="true"></i></div>
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 字体图标 http://www.fontawesome.com.cn/
+// <i class="fa fa-address-book-o" aria-hidden="true"></i>
+import 'font-awesome/css/font-awesome.min.css'
+
 console.log('process.env',process.env);
 
 createApp(App).use(store).use(router).mount('#app')
